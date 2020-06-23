5c4edfee91947d48d6e288dbd9dae366ec02aa34
aboujaafar1997 <aboujaafar.othmane@gmail.com>
2020-06-23 01:40:28 +0100

    add sound & messages

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { useState, useEffect, useRef } from 'react';
 export default function Home(props) {
     const nfc = useRef("");
     const solde = useRef(0);
-
+    const [don, setdon] = useState("SVP mettez votre carte sur le lecteur NFC!");
     const [log, setlog] = useState([]);
     const [totale, settotale] = useState(0);
     const [carte, setcarte] = useState({ nom: "", prenom: "", cin: "", solde: 0, id: "", image: "man.png" });
@@ -19,11 +19,10 @@ export default function Home(props) {
 
         return (
             <>
-                {data.totale > 0 ? <div key={data.carte_id} className="alert alert-success" role="alert">Date:{data.date} Recharge {data.totale} DH pour la Carte {data.carte_id}</div> : <div className="alert alert-danger" role="alert"> Ticket {data.totale} DH  de Carte {data.carte_id}</div>}
+                {data.totale > 0 ? <div key={data.carte_id} className="alert alert-success" role="alert">Date:{data.date} Recharge {data.totale} DH pour la Carte {data.carte_id}</div> : <div className="alert alert-danger" role="alert"> Date:{data.date} Date:{data.date}Ticket {data.totale} DH  de Carte {data.carte_id}</div>}
             </>
         );
     });
-    let first = 0;
     const getticket = async (resultat) => {
         try {
             if (+resultat.solde - 4 > 0) {
@@ -32,13 +31,16 @@ export default function Home(props) {
                 let resultat0 = await response.json();
                 addlog("-4", resultat.id);
                 setcarte({ ...resultat, solde: +resultat.solde - 4 });
-            } else if (first === 0) {
-                return;
+                var audio = new Audio('don.mp3');
+                audio.play();
+                setdon("-4 DH Merci 😃😃");
             } else {
-                alert("solde insuffisante")
+                var audio = new Audio('erreur.mp3');
+                audio.play();
+                setdon("Solde est insuffisant! recheger votre carte svp 😁😁");
             }
         } catch (error) {
-            alert("Erreur de serveur" + error);
+            setdon("Erreur de serveur" + error);
         }
 
     };
@@ -54,7 +56,7 @@ export default function Home(props) {
                 setcarte(resultat[0]);
             }
             else
-                alert("carte non valide !!")
+            setdon("carte non valide !!")
         }
         catch (err) {
             card = "";
@@ -78,10 +80,15 @@ export default function Home(props) {
                 let resultat = await response.json();
                 setcarte({ ...carte, solde: +carte.solde + +solde.current.value });
                 addlog(solde.current.value, carte.id);
+                setdon("Bien recharger avec "+solde.current.value+" DH merci 😃😃")
                 solde.current.value = 0;
+                var audio = new Audio('don.mp3');
+                audio.play();
                 getLog();
             } else {
-                alert("Erreur de solde");
+                setdon("Erreur de solde");
+                var audio = new Audio('erreur.mp3');
+                audio.play();
             }
         } catch (error) {
             card = "";
@@ -108,40 +115,42 @@ export default function Home(props) {
             }
             settotale(s);
             var element = document.getElementById("123");
-            element.scrollTo(0,999999);
+            element.scrollTo(0, 999999);
         }
     }, [log]);
     return (
         <React.Fragment>
             <h6 className="float-right mr-5">Totale:{totale} DH</h6>
-            <div className="row container">
-                <div className="col-7">
-                    <h4>Machine</h4>
-                    <div className="alert alert-dark mt-3 p-5" role="alert">
-                        <button type="button" className="btn btn-outline-dark float-right" onClick={() => { setcarte({ nom: "", prenom: "", cin: "", solde: 0, id: "", image: "man.png" }) }}>Vidé</button>
-                        <div className="row">
-                            <div className="col-6">
-                                <img src={carte.image} alt="image" width="160px" height="160px" />
+            <h1 className="text-center bg-dark text-white p-2">{don}</h1>
+            <div className="container">
+                <div className="row ">
+                    <div className="col-12 col-sm-7">
+                        <h4>Machine</h4>
+                        <div className="alert alert-dark mt-3 p-5" role="alert">
+                            <button type="button" className="btn btn-outline-dark float-right" onClick={() => { setcarte({ nom: "", prenom: "", cin: "", solde: 0, id: "", image: "man.png" }) }}>Vidé</button>
+                            <div className="row">
+                                <div className="col-sm-6 col-12">
+                                    <img src={carte.image} alt="image" width="160px" height="160px" />
+                                </div>
+                                <div className="col-12 col-sm-4 ">
+                                    <h6 className="ofsset-2">solde : {carte.solde}DH</h6>
+                                    <h6>Recharge<input type="number" ref={solde} className="form-control mt-2 float-rightmt-2" /></h6>
+                                    <button type="button" className="btn btn-outline-primary" onClick={() => { recharge() }}>Recharger</button>
+                                </div>
                             </div>
-                            <div className="col-4">
-                                <h6 className="ofsset-2">solde : {carte.solde}DH</h6>
-
-                                <h6>Recharge<input type="number" ref={solde} className="form-control mt-2 float-rightmt-2" /></h6>
-                                <button type="button" className="btn btn-outline-primary" onClick={() => { recharge() }}>Recharger</button>
-                            </div>
+                            <hr />
+                            <div> Nom <input className="float-right form-control col-12" disabled value={carte.nom} /></div><br />
+                            <div> prenom <input className="float-right form-control col-12" disabled value={carte.prenom} /></div><br />
+                            <div> cin <input className="float-right form-control col-12" disabled value={carte.cin} /></div><br />
+                            <div> carte <input className="float-right form-control col-12" disabled value={carte.id} /></div><br />
                         </div>
-                        <hr />
-                        <div> Nom <input className="float-right form-control col-12" disabled value={carte.nom} /></div><br />
-                        <div> prenom <input className="float-right form-control col-12" disabled value={carte.prenom} /></div><br />
-                        <div> cin <input className="float-right form-control col-12" disabled value={carte.cin} /></div><br />
-                        <div> carte <input className="float-right form-control col-12" disabled value={carte.id} /></div><br />
                     </div>
-                </div>
-                <div className="col-5">
-                    <h4>Historique</h4>
-                    <br />
-                    <div id="123" className="bar">
-                        {logs}
+                    <div className="col-sm-5 col-12">
+                        <h4>Historique</h4>
+                        <br />
+                        <div id="123" className="bar">
+                            {logs}
+                        </div>
                     </div>
                 </div>
             </div>
